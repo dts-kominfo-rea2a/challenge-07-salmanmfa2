f5d6cfb8afc8aafd7ca6bdc242ebb1e075840926
salmanmfa2 <salmanmfa2@gmail.com>
2022-06-15 15:03:10 +0800

    feat: challenge7-commit1

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi)=>{
+  try {
+    const arrIXX = await promiseTheaterIXX();
+    const arrVGC = await promiseTheaterVGC();
+    const arrGabung = arrIXX.concat(arrVGC);
+    let acc = 0;
+    for (let index = 0; index < arrGabung.length; index++) {
+      if(arrGabung[index].hasil===emosi){
+        acc++;
+      };
+      
+    }
+    
+
+   
+    return acc;
+
+  }
+  catch(err){
+    
+console.log(err);
+  }
+}
 
 module.exports = {
   promiseOutput,
